e9813a485ba54504240e402e85c0baf5b5c24b84
Haris Gusinac <hgusinac@gmail.com>
2021-02-26 09:05:15 +0100

    stringbuilder etc

--- Program.cs
+++ Program.cs
@@ -18,24 +18,34 @@ namespace Hangman
             Random random = new Random((int)DateTime.Now.Ticks);
             string wordToguess = wordbank[random.Next(0, wordbank.Length)]; // Random Ord att gissa
             string wordToGuessUppercase = wordToguess.ToUpper();
-            StringBuilder displayToPlayer = new StringBuilder(wordToguess.Length);//Se över (wordToGuess.Length)
-            for (int i = 0; i < wordToguess.Length; i++)
-                displayToPlayer.Append('_');
-            List<char> correctGuesses = new List<char>();// Gör om till  Arrays 
-            List<char> incorrectGuesses = new List<char>();// Gör om till String builder Arrays 
-            int lives = 10;
+
+           StringBuilder displayToPlayer = new StringBuilder();//Se över (wordToGuess.Length)
+           for (int i = 0; i < wordToguess.Length; i++)
+             displayToPlayer.Append('_');
+            
+            
+
+
+
+           string[] correctGuesses = new string [10];// Gör om till  Arrays 
+           StringBuilder incorrectGuesses = new StringBuilder();// Gör om till String builder Arrays 
+            
+            int lives = 10; // ändra så att den inte räknar ner samma bokstav 
             bool won = false;
             int lettersRevealed = 0;
             string input;
             char guess;
 
+            StringBuilder wrongWord  = new StringBuilder();
+
             // hur många liv som är kvar synligt !!
+            Console.WriteLine(displayToPlayer);
 
-
-
+           
             while (!won && lives > 0)
             {
-                Console.Write("Guess a letter:");
+                Console.WriteLine("Guess a letter:");
+                
                 input = Console.ReadLine().ToUpper();
                 guess = input[0];
                 if (correctGuesses.Contains(guess))
@@ -45,6 +55,7 @@ namespace Hangman
                 }
                 else if (incorrectGuesses.Contains(guess))
                 {
+                    
                     Console.WriteLine("You already tried '{0}', an it was wrong!", guess);
                     continue;
                 }
@@ -65,14 +76,18 @@ namespace Hangman
                 }
                 else
                 {
-                    incorrectGuesses.Add(guess);
-                    Console.WriteLine("No, there's no '{0}' in the word.", guess);
+                   wrongWord.Append(guess);
+                    Console.WriteLine($"No thats not in the word: \n{wrongWord }.");
+                    Console.WriteLine($"Lives left :{lives}");
                     lives--;
                 }
                 Console.WriteLine(displayToPlayer.ToString());
             }
             if (won)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("You won!");
+            }
 
             else
                 Console.WriteLine("You lose! it was '{0}'", wordToguess);
