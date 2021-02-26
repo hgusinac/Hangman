1d452edc7e2e77c9632a30b1462114acc84221a6
Haris Gusinac <hgusinac@gmail.com>
2021-02-26 10:18:13 +0100

    nu kan du gissa på hela ordet

--- Program.cs
+++ Program.cs
@@ -18,70 +18,86 @@ namespace Hangman
             Random random = new Random((int)DateTime.Now.Ticks);
             string wordToguess = wordbank[random.Next(0, wordbank.Length)]; // Random Ord att gissa
             string wordToGuessUppercase = wordToguess.ToUpper();
+            StringBuilder displayToPlayer = new StringBuilder();//Se över (wordToGuess.Length)
+            for (int i = 0; i < wordToguess.Length; i++)
+                displayToPlayer.Append('_');
 
-           StringBuilder displayToPlayer = new StringBuilder();//Se över (wordToGuess.Length)
-           for (int i = 0; i < wordToguess.Length; i++)
-             displayToPlayer.Append('_');
-            
-            
+            char[] correctGuesses = new char[10];// Gör om till  Arrays 
 
+            StringBuilder incorrectGuesses = new StringBuilder();// fel gissningar
 
+            // List<char> incorrectGuesses = new List<char>();// Gör om till String builder Arrays 
 
-           string[] correctGuesses = new string [10];// Gör om till  Arrays 
-           StringBuilder incorrectGuesses = new StringBuilder();// Gör om till String builder Arrays 
-            
-            int lives = 10; // ändra så att den inte räknar ner samma bokstav 
+            int lives = 9;
             bool won = false;
             int lettersRevealed = 0;
             string input;
             char guess;
 
-            StringBuilder wrongWord  = new StringBuilder();
+
 
             // hur många liv som är kvar synligt !!
             Console.WriteLine(displayToPlayer);
 
-           
+
             while (!won && lives > 0)
             {
-                Console.WriteLine("Guess a letter:");
-                
+                Console.Write("Guess a letter:");
                 input = Console.ReadLine().ToUpper();
-                guess = input[0];
-                if (correctGuesses.Contains(guess))
+                if (input.Length > 1)
                 {
-                    Console.WriteLine("You already tried '{0}' and it was correct!", guess);
-                    continue;
-                }
-                else if (incorrectGuesses.Contains(guess))
-                {
-                    
-                    Console.WriteLine("You already tried '{0}', an it was wrong!", guess);
-                    continue;
-                }
-                if (wordToGuessUppercase.Contains(guess))
-                {
-                    correctGuesses.Add(guess);
-                    for (int i = 0; i < wordToguess.Length; i++)
+                    lives--;
+                    if (input.Equals(wordToguess))
                     {
-                        if (wordToGuessUppercase[i] == guess)
+                        for (int i = 0; i < wordToguess.Length; i++)
                         {
                             displayToPlayer[i] = wordToguess[i];
                             lettersRevealed++;
                         }
-
                     }
-                    if (lettersRevealed == wordToGuessUppercase.Length)
-                        won = true;
+                    won = true;
                 }
-                else
+                if (input.Length == 1)
                 {
-                   wrongWord.Append(guess);
-                    Console.WriteLine($"No thats not in the word: \n{wrongWord }.");
-                    Console.WriteLine($"Lives left :{lives}");
-                    lives--;
+
+
+                    guess = input[0];
+
+                    if (correctGuesses.Contains(guess))
+                    {
+                        Console.WriteLine("You already tried '{0}' and it was correct!", guess);
+                        continue;
+                    }
+                    else if (incorrectGuesses.ToString().Contains(guess))
+                    {
+
+                        Console.WriteLine("You already tried '{0}', an it was wrong!", guess);
+                        continue;
+                    }
+                    if (wordToGuessUppercase.Contains(guess))
+                    {
+                        correctGuesses.Append(guess);
+                        for (int i = 0; i < wordToguess.Length; i++)
+                        {
+                            if (wordToGuessUppercase[i] == guess)
+                            {
+                                displayToPlayer[i] = wordToguess[i];
+                                lettersRevealed++;
+                            }
+
+                        }
+                        if (lettersRevealed == wordToGuessUppercase.Length)
+                            won = true;
+                    }
+                    else
+                    {
+                        incorrectGuesses.Append(guess);
+                        Console.WriteLine($"No, there's no '{incorrectGuesses} ' in the word.");
+                        Console.WriteLine($"Lives left :{lives}");
+                        lives--;
+                    }
+                    Console.WriteLine(displayToPlayer.ToString());
                 }
-                Console.WriteLine(displayToPlayer.ToString());
             }
             if (won)
             {
@@ -97,6 +113,7 @@ namespace Hangman
         }
     }
 }
+
 
 
 
